01728283e46de98492e0f54a7202a036ff5516f5
mdelage <angie-island@hotmail.fr>
2024-11-26 15:40:53 +0100

    détail d'un film

---
+++ src/main/java/fr/apside/DVDStore/DVDStoreWeb/controller/MovieController.java
@@ -0,0 +1,24 @@
+package fr.apside.DVDStore.DVDStoreWeb.controller;
+
+import fr.apside.DVDStore.core.service.MovieServiceInterface;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movie")
+public class MovieController {
+
+	private final MovieServiceInterface movieServiceInterface;
+
+	public MovieController(MovieServiceInterface movieServiceInterface) {
+		this.movieServiceInterface = movieServiceInterface;
+	}
+
+	@RequestMapping("/{id}")
+	public String displayMovieCard(@PathVariable("id") int id, Model model){
+		model.addAttribute("movie", movieServiceInterface.getMovieById(id));
+		return "movie-details";
+	}
+}
